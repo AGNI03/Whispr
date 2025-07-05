de76d895e537d01f5e666878480fe0b523d5e0e3
Vishal <vishalanand1033@gmail.com>
2025-07-05 22:19:29 +0530

    added message.route.js and message.controller.js, Updated index.js for the new route, added two routes getUserForSidebar and getMessages

---
+++ server/src/routes/message.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { protectRoute } from '../middlewares/auth.middleware.js';
+import { getMessages, getUsersForSidebar } from '../controllers/message.contoller.js';
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+
+export default router;
